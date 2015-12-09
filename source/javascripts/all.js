7c7aa56e47ea5115876ed1eee8ecc3591c7a038d
cmaxwell <christopher-maxwell@hotmail.com>
2015-12-09 16:51:59 +1100

    updated placeholder images in risk section

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
   $('#conservative').on('click', function(e) {
     e.preventDefault();
-    $("#risk-profile-chart").attr("src","/images/asset_classes/cash/1.svg");
+    $("#risk-profile-chart").attr("src","/images/risk/conservative.svg");
     $("#risk-profile-title").text("Conservative portfolio");
     $('#conservative').addClass('selected');
     $('#balanced').removeClass('selected');
@@ -11,7 +11,7 @@ $(document).ready(function() {
 
   $('#balanced').on('click', function(e) {
     e.preventDefault();
-    $("#risk-profile-chart").attr("src","/images/asset_classes/cash/2.svg");
+    $("#risk-profile-chart").attr("src","/images/risk/balanced.svg");
     $("#risk-profile-title").text("Balanced portfolio");
     $('#conservative').removeClass('selected');
     $('#balanced').addClass('selected');
@@ -20,7 +20,7 @@ $(document).ready(function() {
 
   $('#growth').on('click', function(e) {
     e.preventDefault();
-    $("#risk-profile-chart").attr("src","/images/asset_classes/cash/3.svg");
+    $("#risk-profile-chart").attr("src","/images/risk/growth.svg");
     $("#risk-profile-title").text("Growth portfolio");
     $('#conservative').removeClass('selected');
     $('#balanced').removeClass('selected');
