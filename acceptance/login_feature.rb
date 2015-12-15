feefd6e0519c6039d83a4dd73b0395413c5ed5ea
Isabella Li <dnflyangle@gmail.com>
2015-12-15 17:20:36 +1100

    fix the static url thing in testing

--- acceptance/login_feature.rb
+++ acceptance/login_feature.rb
@@ -3,7 +3,7 @@ describe 'Login' do
   describe 'with valid password' do
 
     it 'should direct to home page' do
-      visit 'http://localhost:8081/login'
+      visit '/login'
       fill_in('password', :with => 'test123')
       find('.login-button').click
       expect(page).to have_content('Wealth iQ')
