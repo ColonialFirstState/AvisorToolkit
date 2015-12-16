d599ec996ab05f39056fbabcdd40409b52b7edbe
Isabella Li <dnflyangle@gmail.com>
2015-12-16 16:34:36 +1100

    add acceptance test for cash page

---
+++ acceptance/asset_classes/cash_feature.rb
@@ -0,0 +1,42 @@
+require_relative '../../acceptance/login_helper'
+
+describe 'Cash Page' do
+  include_context 'valid login'
+
+  before (:each) do
+    login
+    visit '/asset_classes/cash'
+  end
+
+  describe 'Navigation' do
+
+    it 'should select cash in second navigation level' do
+      expect(find('.second-level .nav .selected a').text).to eq('Cash')
+    end
+
+    it 'should direct to risk profile when click previous button' do
+      find('div.previous').click
+      expect(current_url).to include('/risk/profile')
+    end
+
+    it 'should direct to fixed income page when click next button' do
+      find('div.next').click
+      expect(current_url).to include('/asset_classes/fixed_income')
+    end
+  end
+
+  describe 'Content' do
+
+    it 'should have correct title' do
+      expect(find('.explaination .title p').text).to eq('An example of investing in cash')
+    end
+
+    it 'should have correct step explanation' do
+      titles = all('.steps .st1 p')
+
+      expect(titles[0].text).to eq('You loan money to a bank in the form of a term deposit')
+      expect(titles[1].text).to eq('The bank then lends money to borrowers and charges them interest on those loans')
+      expect(titles[2].text).to eq('Your deposit may receive a return in the form of interest plus you receive your initial loan at the end of the term')
+    end
+  end
+end
