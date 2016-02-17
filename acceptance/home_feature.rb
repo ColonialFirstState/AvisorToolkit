50d960bb751509810726f3f938a1075e30014777
Venus Izadi <venus.izadi@gmail.com>
2016-02-17 15:02:39 +1100

    [Venus & Ray] adding Retirement box to the main page

--- acceptance/home_feature.rb
+++ acceptance/home_feature.rb
@@ -24,6 +24,7 @@ describe 'Home Page' do
       expect(find('.managed-funds .section-title').text).to eq('Managed Funds')
       expect(find('.super .section-title').text).to eq('Super')
       expect(find('.supporting-content .section-title').text).to eq('Supporting Content')
+      expect(find('.retirement .section-title').text).to eq('Retirement')
     end
 
     it 'risk button should link to risk profile page' do
@@ -49,6 +50,11 @@ describe 'Home Page' do
     it 'supporting content button should link to supporting content page' do
       find('div.supporting-content').click
       expect(current_url).to include('/supporting_content')
+    end
+
+    it 'retirement button should link to retirement page' do
+      find('div.retirement').click
+      expect(current_url).to include('/retirement')
     end
 
   end
