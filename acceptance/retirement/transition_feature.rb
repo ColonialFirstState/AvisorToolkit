59eea1312b81e53fbf45e11142e15f717e0a659f
Venus Izadi <venus.izadi@gmail.com>
2016-02-18 12:36:25 +1100

    [Venus] Adding next/previous button to Transition page

--- acceptance/retirement/transition_feature.rb
+++ acceptance/retirement/transition_feature.rb
@@ -1,4 +1,4 @@
-require_relative '../acceptance/login_helper'
+require_relative '../../acceptance/login_helper'
 
 describe 'Retirement Page' do
   include_context 'valid login'
@@ -9,7 +9,23 @@ describe 'Retirement Page' do
   end
 
   it 'should have the correct title' do
-    expect(find('div.title').text).to eq('Transition to Retirement')
+    expect(find('p.title').text).to eq('Transition to Retirement')
+  end
+
+  describe 'Next Button' do
+
+    it 'should direct to Income page when click' do
+      find('div.next').click
+      expect(current_url).to include('/retirement/income')
+    end
+  end
+
+  describe 'Previous Button' do
+
+    it 'should direct to Super when click' do
+      find('div.previous').click
+      expect(current_url).to include('/super')
+    end
   end
 
 end
