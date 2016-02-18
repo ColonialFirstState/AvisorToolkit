dead2b9ce6c988f0e65525b193ca8fac2f7a388d
Venus Izadi <venus.izadi@gmail.com>
2016-02-18 12:35:44 +1100

    [Venus] Add previous/next button to income page

--- acceptance/retirement/income_feature.rb
+++ acceptance/retirement/income_feature.rb
@@ -1,6 +1,6 @@
-require_relative '../acceptance/login_helper'
+require_relative '../../acceptance/login_helper'
 
-describe 'Retirement Page' do
+describe 'Income in Retirement Page' do
   include_context 'valid login'
 
   before (:each) do
@@ -9,7 +9,22 @@ describe 'Retirement Page' do
   end
 
   it 'should have the correct title' do
-    expect(find('div.title').text).to eq('Income in Retirement')
+    expect(find('p.title').text).to eq('Income in Retirement')
   end
 
+  describe 'Next Button' do
+
+    it 'should direct to Supporting Content page when click' do
+      find('div.next').click
+      expect(current_url).to include('/supporting_content')
+    end
+  end
+
+  describe 'Previous Button' do
+
+    it 'should direct to Transition page when click' do
+      find('div.previous').click
+      expect(current_url).to include('/retirement/transition')
+    end
+  end
 end
