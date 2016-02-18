d44a9a7e4b9201777e07adfd92e5747e6ede5ee4
Ray Tung <rtung@thoughtworks.com>
2016-02-18 11:48:51 +1100

    Refactored risk.js and some css/DOM logic that tied together with it

--- source/javascripts/risk.js
+++ source/javascripts/risk.js
@@ -1,36 +1,16 @@
 $(document).ready(function() {
-  $('#conservative').on('click', function(e) {
-    e.preventDefault();
-    $(".medium-risk").attr('class', 'rating-no medium-risk');
-    $(".high-risk").attr('class', 'rating-no high-risk');
-    $(".low-risk").attr('class', 'rating-yes low-risk');
-    $("#risk-profile-title").text("Conservative portfolio");
-    $('#conservative').addClass('selected');
-    $('#balanced').removeClass('selected');
-    $('#growth').removeClass('selected');
-  });
-
-  $('#balanced').on('click', function(e) {
-    e.preventDefault();
-    $(".medium-risk").attr('class', 'rating-yes medium-risk');
-    $(".high-risk").attr('class', 'rating-no high-risk');
-    $(".low-risk").attr('class', 'rating-no low-risk');
-    $("#risk-profile-title").text("Conservative portfolio");
-    $("#risk-profile-title").text("Balanced portfolio");
-    $('#conservative').removeClass('selected');
-    $('#balanced').addClass('selected');
-    $('#growth').removeClass('selected');
-  });
-
-  $('#growth').on('click', function(e) {
-    e.preventDefault();
-    $(".medium-risk").attr('class', 'rating-no medium-risk');
-    $(".high-risk").attr('class', 'rating-yes high-risk');
-    $(".low-risk").attr('class', 'rating-no low-risk');
-    $("#risk-profile-title").text("Conservative portfolio");
-    $("#risk-profile-title").text("Growth portfolio");
-    $('#conservative').removeClass('selected');
-    $('#balanced').removeClass('selected');
-    $('#growth').addClass('selected');
-  });
+  buttonClickAction('#conservative', '.low-risk',    'Conservative portfolio');
+  buttonClickAction('#balanced',     '.medium-risk', 'Balanced portfolio');
+  buttonClickAction('#growth',       '.high-risk',   'Growth portfolio');
 });
+
+function buttonClickAction(button, risk, titleText) {
+  $(button).on('click', function(e) {
+    e.preventDefault();
+    $('.rating').removeClass('rating-yes');
+    $('.button').removeClass('selected');
+    $(button).addClass('selected');
+    $(risk).addClass('rating-yes');
+    $('#risk-profile-title').text(titleText);
+  });
+}
