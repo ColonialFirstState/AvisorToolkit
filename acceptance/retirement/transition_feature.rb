4de003a2752ab358894473fdc65df39751ae7897
Venus Izadi <venus.izadi@gmail.com>
2016-02-25 11:26:55 +1100

    Finished TTR benefits tab

--- acceptance/retirement/transition_feature.rb
+++ acceptance/retirement/transition_feature.rb
@@ -20,6 +20,37 @@ describe 'Retirement Page' do
     end
   end
 
+  describe 'Benefits' do
+    it 'should have a correct sub-title' do
+      expect(find('p.step.st1').text).to eq('Some of the benefits of a transition to retirement strategy:')
+    end
+
+    it 'should have correct src for boost income' do
+      expect(find('#boost-income')['src'].include?('/images/retirement/transition_to_retirement/boost_income.svg')).to be_true
+    end
+
+    it 'should have correct src for tax saving on earnings' do
+      expect(find('#tax-saving-on-earnings')['src'].include?('/images/retirement/transition_to_retirement/tax_saving_on_earnings.svg')).to be_true
+    end
+
+    it 'should have correct src for tax savings on income' do
+      expect(find('#tax-saving-on-income')['src'].include?('/images/retirement/transition_to_retirement/tax_saving_on_income.svg')).to be_true
+    end
+
+    it 'should have correct text for boost income' do
+      expect(find('#boost-income-text').text).to eq('Maintain or boost your income')
+    end
+
+    it 'should have correct text for tax saving on earnings' do
+      expect(find('#tax-saving-on-earnings-text').text).to eq('Minimise tax on earnings')
+    end
+
+    it 'should have correct text for tax savings on income' do
+      expect(find('#tax-saving-on-income-text').text).to eq('Minimise tax on income')
+    end
+
+  end
+
   describe 'Previous Button' do
 
     it 'should direct to Super when click' do
